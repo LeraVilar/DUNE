f597a5cf2c1d4f0894319d644b31509024bc5fa5
LeraVilar <69603826+LeraVilar@users.noreply.github.com>
2022-01-12 12:04:59 +0400

    9

---
+++ src/scripts/support.js
@@ -0,0 +1,27 @@
+let nav = document.querySelectorAll('.support__item'),
+    sub_nav_parent = document.querySelectorAll('.support__card-parent'),
+    sub_nav = document.querySelectorAll('.support__card'),
+    end_nav = document.querySelectorAll('.support__content');
+
+nav.forEach(element => {
+    element.addEventListener('click', function(){
+        let data_nav = element.dataset.index;
+        sub_nav.forEach(el => {
+            el.classList.remove('active');
+            let data_sub = el.dataset.parent;
+            if(data_nav == data_sub) {
+                el.classList.add('active');
+            }
+            el.addEventListener('click', function(){
+                let data_child = el.dataset.child;
+                end_nav.forEach(e => {
+                    e.classList.remove('active');
+                    let data_end = e.dataset.children;
+                    if(data_child == data_end) {
+                        e.classList.add('active');
+                    }
+                });
+            })
+        });
+    })
+});
