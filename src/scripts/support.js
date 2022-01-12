db204a33379f14b443350879f820bda067e53b72
LeraVilar <69603826+LeraVilar@users.noreply.github.com>
2022-01-12 12:59:54 +0400

    9

--- src/scripts/support.js
+++ src/scripts/support.js
@@ -1,13 +1,22 @@
 let nav = document.querySelectorAll('.support__item'),
-    sub_nav_parent = document.querySelectorAll('.support__card-parent'),
+    sub_nav_parent = document.querySelectorAll('.support__contents'),
     sub_nav = document.querySelectorAll('.support__card'),
     end_nav = document.querySelectorAll('.support__content');
+
+function deb(){
+    end_nav.forEach(act => {
+        if(act.classList.contains('active')) {
+            act.classList.remove('active');
+        }
+    });
+}
 
 nav.forEach(element => {
     element.addEventListener('click', function(){
         let data_nav = element.dataset.index;
         sub_nav.forEach(el => {
             el.classList.remove('active');
+            deb();
             let data_sub = el.dataset.parent;
             if(data_nav == data_sub) {
                 el.classList.add('active');
